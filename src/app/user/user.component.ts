977eea859e8a73e71afad429cc9f751db35ec84c
Lucas Racoci <racoci.0@gmail.com>
2025-01-21 12:24:18 -0300

    Add and bind dummy users

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+
+const randomIndex = Math.floor(
+  Math.random() * DUMMY_USERS.length
+);
 
 @Component({
   selector: 'app-user',
@@ -7,6 +12,7 @@ import { Component } from '@angular/core';
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
-export class UserComponent {
 
+export class UserComponent {
+  selectedUser = DUMMY_USERS[randomIndex];
 }
