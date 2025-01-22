af5b78001bd4fa2a54e7d727ed2623f913660ff9
Lucas Racoci <racoci.0@gmail.com>
2025-01-22 18:17:16 -0300

    Create and use shared module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,5 @@
 import {Component} from '@angular/core';
-import {HeaderComponent} from './header/header.component';
-import {UserComponent} from "./user/user.component";
 import {DUMMY_USERS} from './dummy-data';
-import {TasksComponent} from './tasks/tasks.component';
-import {NgFor, NgIf} from "@angular/common"
 import {UserModel} from "./user/user.model";
 
 
